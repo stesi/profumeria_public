525945f2d386d96239fd7339b4156975e9bc6043
Vincenzo Di Roberto <diroberto.v@stesi.eu>
2021-01-27 13:09:47 +0100

    Logger info

--- wizard/import_order.py
+++ wizard/import_order.py
@@ -16,6 +16,7 @@ _logger = logging.getLogger(__name__)
 class ImportOrder(models.TransientModel):
     _name = "profumeria.public.import.order"
     _description = 'Import Order and OrderLine from XML'
+
     # file_data = fields.Binary('File')
     #
     # def action_check_wizard(self):
@@ -104,9 +105,9 @@ class ImportOrder(models.TransientModel):
                         nb_print = number_tag.text
 
                     order = self.env['pos.order'].create({
-                        'name': warehouse.text+"/"+nb_print,
+                        'name': warehouse.text + "/" + nb_print,
                         'company_id': company_id,
-                        'pos_reference':nb_print,
+                        'pos_reference': nb_print,
                         'date_order': date_order,
                         'partner_id': partner,
                         'session_id': session.id,
@@ -128,7 +129,8 @@ class ImportOrder(models.TransientModel):
                             product_text = 'Info'
                         product_id = self.env['product.product'].search([('default_code', '=', product_text)], limit=1)
                         if not product_id.id:
-                            product_category_id = self.env['product.category'].search([('name','=','Imported')],limit=1)
+                            product_category_id = self.env['product.category'].search([('name', '=', 'Imported')],
+                                                                                      limit=1)
                             if not product_category_id:
                                 product_category_id = self.env['product.category'].create({
                                     'name': 'Imported',
@@ -136,7 +138,7 @@ class ImportOrder(models.TransientModel):
 
                             product_id = self.env['product.product'].create(
                                 {'name': product_text, 'default_code': product_text,
-                                 'categ_id':product_category_id.id,
+                                 'categ_id': product_category_id.id,
                                  'company_id': self.env.user.company_id.id, })
                             # _logger.warning(
                             #     "Product section value not found for document " + str(i) + " row " + str(j)+ ' sourceline ' + str(document.sourceline))
@@ -159,15 +161,14 @@ class ImportOrder(models.TransientModel):
                         account_tax = False
                         subtotal = 0
                         vatTag = row.find('VatCode')
-                        vat=False
+                        vat = False
                         if vatTag.text:
                             try:
                                 vat = float(vatTag.text)
                                 account_tax = self.env['account.tax'].search([('amount', '=', vat),
                                                                               ('type_tax_use', '=', 'sale')], limit=1)
                             except ValueError:
-                                    pass
-
+                                pass
 
                         subtotal = 0
                         total = 0
@@ -175,7 +176,7 @@ class ImportOrder(models.TransientModel):
                         if totalTag.text:
                             total = float(totalTag.text)
                             if vat > 0:
-                                subtotal = round((total*100) / (100+vat),2)
+                                subtotal = round((total * 100) / (100 + vat), 2)
 
                         discount = False
                         discountTag = row.find('Discounts')
@@ -224,13 +225,13 @@ class ImportOrder(models.TransientModel):
                     posMakePayment = self.env['pos.make.payment']
                     # I make a payment to fully pay the order
                     context_make_payment = {"active_ids": [order.id],
-                                                "active_id": order.id}
+                                            "active_id": order.id}
                     pos_make_payment_0 = posMakePayment.with_context(context_make_payment).create(
-                            {
-                                'amount': order.amount_total,
-                                'payment_method_id': payment_method.id,
-                                'payment_name': payment
-                            })
+                        {
+                            'amount': order.amount_total,
+                            'payment_method_id': payment_method.id,
+                            'payment_name': payment
+                        })
                     # print(pos_make_payment_0.id)
                     # I click on the validate button to register the payment.
                     context_payment = {'active_id': order.id}
@@ -241,6 +242,171 @@ class ImportOrder(models.TransientModel):
                     raise ValidationError(
                         "Warehouse section value not found for document " + str(i) + ' sourceline ' + str(
                             document.sourceline))
+
+            except ValidationError as inst:
+                _logger.warning("Line " + str(document.sourceline) + str(inst))
+                print("Line " + str(document.sourceline) + str(inst))
+
+            except Exception as inst:
+                _logger.error("Line " + str(document.sourceline) + str(inst))
+                print("Line " + str(document.sourceline) + str(inst))
+
+    #not working
+    @api.model
+    def importInvoiceFromDirectoryXML(self, filename):
+        company_id = self.env.user.company_id.id
+
+        xml = etree.parse(filename)
+        root = xml.getroot()
+        documentsHeader = root.find('Documents')
+        documents = documentsHeader.findall('Document')
+        customerTag = False
+        i = 0
+        _logger.info(str(len(documents)))
+        for document in documents:
+            try:
+
+                i += 1
+                print("Number document " + str(i) + " line " + str(document.sourceline))
+                _logger.info("Number document " + str(i) + " line " + str(document.sourceline))
+                customerTag = document.find('CustomerCode')
+                partner = False
+                if customerTag is not None and customerTag.text:
+                    partner_id = self.env['res.partner'].search([('ref', '=', customerTag.text)], limit=1)
+                    if not partner_id.id:
+                        #raise ValidationError("Partner " + customerTag.text + " not found for document " + str(i))
+                        partner_id = self.env['res.partner'].create({
+                                'name': customerTag.text,
+                                'company_id': self.env.user.company_id.id,
+                                'ref': customerTag.text
+                            })
+                        partner = partner_id.id
+
+                    date_tag = document.find('Date')
+                    if date_tag is not None and date_tag.text:
+                        date_order = datetime.datetime.strptime(date_tag.text, "%Y-%m-%d")
+                    else:
+                        raise ValidationError(
+                            "Date section value not found for document " + str(i) + ' sourceline ' + str(
+                                document.sourceline))
+
+                    total = 0
+                    totalTag = document.find('TotalWithoutTax')
+                    if totalTag.text:
+                        total = float(totalTag.text)
+
+                    vat_amount = document.find('VatAmount')
+                    amount_tax = 0
+                    if vat_amount.text:
+                        amount_tax = float(vat_amount.text)
+
+                    name = False
+                    number_tag = document.find('Number')
+                    if number_tag.text:
+                        name = number_tag.text
+                    number_tag = document.find('Numbering')
+                    if number_tag.text:
+                        name += number_tag.text
+
+                    order = self.env['account.move'].create({
+                        'name': name,
+                        'company_id': company_id,
+                        'invoice_date': date_order,
+                        'partner_id': partner,
+                        'amount_total': total,
+                        'amount_tax': amount_tax,
+                        'move_type': 'out_invoice'
+                    })
+
+                    rowsHeader = document.find('Rows')
+                    rows = rowsHeader.findall('Row')
+                    j = 0
+                    for row in rows:
+                        j += 1
+                        code = row.find('Code')
+                        product_text = code.text
+                        if not product_text:
+                            product_text = 'Info'
+                        product_id = self.env['product.product'].search([('default_code', '=', product_text)], limit=1)
+                        if not product_id.id:
+                            product_category_id = self.env['product.category'].search([('name', '=', 'Imported')],
+                                                                                      limit=1)
+                            if not product_category_id:
+                                product_category_id = self.env['product.category'].create({
+                                    'name': 'Imported',
+                                })
+
+                            product_id = self.env['product.product'].create(
+                                {'name': product_text, 'default_code': product_text,
+                                 'categ_id': product_category_id.id,
+                                 'company_id': self.env.user.company_id.id, })
+                            # _logger.warning(
+                            #     "Product section value not found for document " + str(i) + " row " + str(j)+ ' sourceline ' + str(document.sourceline))
+                            # continue
+
+                        name = False
+                        description = row.find('Description')
+                        if description.text:
+                            name = description.text
+
+                        quantity = 0
+                        quantityTag = row.find('Qty')
+                        if quantityTag.text:
+                            quantity = float(quantityTag.text)
+
+                        price = 0
+                        priceTag = row.find('Price')
+                        if priceTag.text:
+                            price = float(priceTag.text)
+                        account_tax = False
+                        subtotal = 0
+                        vatTag = row.find('VatCode')
+                        vat = False
+                        if vatTag.text:
+                            try:
+                                vat = float(vatTag.text)
+                                account_tax = self.env['account.tax'].search([('amount', '=', vat),
+                                                                              ('type_tax_use', '=', 'sale')], limit=1)
+                            except ValueError:
+                                pass
+
+                        subtotal = 0
+                        total = 0
+                        totalTag = row.find('Total')
+                        if totalTag.text:
+                            total = float(totalTag.text)
+                            if vat > 0:
+                                subtotal = round((total * 100) / (100 + vat), 2)
+
+                        discount = False
+                        discountTag = row.find('Discounts')
+
+                        if discountTag.text:
+                            discount = discountTag.text.replace("%", "")
+                            total_temp = price
+                            if '+' in discount:
+                                pair_list = discount.split("+")
+                                discount_temp = 0
+                                for disc in pair_list:
+                                    discount_temp = (total_temp * float(disc) / 100) + discount_temp
+                                    total_temp = total_temp - discount_temp
+                                discount = discount_temp
+                            else:
+                                discount = price * float(discount) / 100
+
+                        #'invoice_line_ids': [(0, 0, {'product_id': ipad.id, 'quantity': 20.0, 'price_unit': 750.0})],
+                        order.invoice_line_ids|=[(0, 0,{
+                            'product_id': product_id.id,
+                            'price_unit': price,
+                            'discount': discount,
+                            'name': name,
+                            'quantity': quantity,
+                            'price_subtotal': subtotal,
+                            'price_total': total
+                        })]
+                        # if account_tax:
+                        #     line.tax_ids |= account_tax
+
 
             except ValidationError as inst:
                 _logger.warning("Line " + str(document.sourceline) + str(inst))
