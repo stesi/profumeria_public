a2d810384104bf3cf73ef5534434392167d27daa
Arcadio <pinto.a@stesi.eu>
2021-02-19 15:40:30 +0100

    update

--- __manifest__.py
+++ __manifest__.py
@@ -25,7 +25,6 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'data/base_automation.xml',
         'views/view_pos_order.xml'
     ],
     # only loaded in demonstration mode
