b5d173686ef9f7806949d1b7593564643495d46d
Arcadio <pinto.a@stesi.eu>
2021-02-19 18:56:08 +0100

    update

--- models/purchase_order_line.py
+++ models/purchase_order_line.py
@@ -5,4 +5,4 @@ from odoo import api, fields, models
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
-    brand = fields.Char(related='product_template_id.brand', store=True)
+    brand = fields.Char(related='product_id.brand', store=True)
