9ad471174961b9669ca9ced2e2f94a96ff0775e1
Arcadio <pinto.a@stesi.eu>
2021-02-19 18:00:03 +0100

    update

--- models/product_template.py
+++ models/product_template.py
@@ -4,4 +4,4 @@ from odoo import api, fields, models, _
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    brand = fields.Char(related='x_producer', store=True)
+    brand = fields.Char(store=True)
