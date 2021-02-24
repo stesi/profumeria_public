844be08f2796ce130f159252ec960bcbffb3f66a
Arcadio <pinto.a@stesi.eu>
2021-02-24 09:51:46 +0100

    update

--- models/product_template.py
+++ models/product_template.py
@@ -5,3 +5,11 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     brand = fields.Char(store=True)
+
+    @api.model
+    def fields_get(self, fields=None):
+        fields_to_hide = ['product_brand_ept_id']
+        res = super(ProductTemplate, self).fields_get()
+        for field in fields_to_hide:
+            res[field]['searchable'] = False
+        return res
