6226475cb40675e7b46d22bc2e006c85145e43e9
Arcadio <pinto.a@stesi.eu>
2021-03-12 14:40:46 +0100

    update

--- __manifest__.py
+++ __manifest__.py
@@ -21,7 +21,7 @@
     'version': '14.0.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['point_of_sale', 'base_automation', 'sale', 'purchase', 'point_of_sale','pos_margin', 'l10n_it_delivery_note'],
+    'depends': ['point_of_sale', 'base_automation', 'sale', 'purchase', 'point_of_sale','pos_margin', 'l10n_it_delivery_note', 'emipro_theme_base'],
     # always loaded
     'data': [
         'security/ir.model.access.csv',
--- models/product_template.py
+++ models/product_template.py
@@ -4,5 +4,10 @@ from odoo import api, fields, models, _
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    brand = fields.Char(store=True)
+    brand = fields.Char(compute="_compute_brand")
+
+    @api.depends("product_brand_ept_id")
+    def _compute_brand(self):
+        for product in self:
+            product.brand = str(product.product_brand_ept_id.name)
 
