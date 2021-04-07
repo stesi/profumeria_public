bc169551eb6c05373291ca61cdfd579bec41e728
Vincenzo Di Roberto <diroberto.v@stesi.eu>
2021-04-07 15:38:25 +0200

    modificato product product

--- models/product_product.py
+++ models/product_product.py
@@ -5,3 +5,6 @@ class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     brand = fields.Char(related='product_tmpl_id.brand', store=True)
+
+    #one2many reward_ids = fields.One2Many 'loyalty.reward' ('product.product', string='Gift Product', help='The product given as a reward')
+    #minum point è computato dalla one2many
