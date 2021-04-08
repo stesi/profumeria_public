2aa1b8e4a75d4b3c7dc172651a4eab873f5b412f
Francesco Pranzo <pranzo.f@stesi.eu>
2021-04-08 12:36:00 +0200

    in product.product added relation with loyalty.reward

--- __manifest__.py
+++ __manifest__.py
@@ -27,6 +27,7 @@
         'security/ir.model.access.csv',
         'views/view_pos_order.xml',
         'views/product_template.xml',
+        'views/product_product.xml',
         'views/pos_order_view.xml',
         'views/purchase_order_view.xml',
         'views/sale_order_view.xml',
